14d351f191614bce33909611cec5520fec6c3672
Russ2good <55982245+Russ2good@users.noreply.github.com>
2019-11-13 10:37:40 +0100

    Fixet Dice og addet startGame metode

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Player {
+
+    public static String[] startGame(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Velkommen til spillet, hvor mange spillere er i?");
+        int antalSpilere = scan.nextInt();
+        String[] names = new String[antalSpilere];
+        for (int i = 0; i < antalSpilere; i++) {
+            System.out.println(" Fedt! Spiller " + (i + 1) + ", hvad er dit navn");
+            names[i] = scan.nextLine();
+            System.out.println("Hej " + names[i] + "!");
+            System.out.println("------------------------------------------------");
+        }
+        System.out.println("Lad spillet begynde!");
+        System.out.println("Alle spillere starter med en balance på 1000");
+        System.out.println("------------------------------------------------");
+        return names;
+    }
 }
