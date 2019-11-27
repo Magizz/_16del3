67ec514e8937c5b5c604ce998d8c8cfcff515e69
nikolaiwegge-berg <nikolaiweggeberg@yahoo.dk>
2019-11-27 14:39:36 +0100

    Merge branch 'Development' of /Users/nikolaiwegge-berg/Documents/_16del3 with conflicts.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,16 @@
 package main;
 
 import Controller.GameController;
-import Controller.MainController;
 import Controller.UIController;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        MainController m = new MainController();
-        m.runTest();
+       main.Test test = new main.Test();
+       test.mainTest();
+      //test.gameLoop();
+
         //UIController UI = new UIController();
 
         //MainController main = new MainController();
--- src/main/Player.java
+++ src/main/Player.java
@@ -4,15 +4,14 @@ import Tiles.Property;
 import com.sun.deploy.panel.IProperty;
 
 public class Player {
+    //Bal skal hedde noget andet (balance)
     private static int MaxProperties = 12;
-    private String name;
-    private Bank playerBank = new Bank();
-    private int age;
-    private int tilePosition;
+     private String name;
+     private Bank bal = new Bank();
+     private int age;
+     private int tilePosition;
     private int numberPropertiesOwned = 0;
     private Property ownedProperty[] = new Property[MaxProperties];
-    private boolean jailFreeCard = false;
-
 
     public Player () {
         name = "";
@@ -23,27 +22,25 @@ public class Player {
     }
 
     public Player (String name, int age, int initialMoney) {
-        playerBank.setMoney(initialMoney);
+        bal.setMoney(initialMoney);
         this.name = name;
         this.age = age;
         tilePosition = 0;
     }
 
-    // Accessors
+    //Accessor
     public String getName() { return name; }
-    public int getBal() { return playerBank.getMoney(); }
+    public int getBal() { return bal.getMoney(); }
     public int getAge() { return age; }
     public int getTilePosition() { return tilePosition; }
-    public boolean getJailFreeCard() {return jailFreeCard; }
 
-    // Mutators
+    //Mutator
     public void setTilePosition( int tp ) { tilePosition = tp; }
     public void setName(String n) { name = n; }
     public void setAge(int a) { age = a; }
-    public void setBal(int b) { playerBank.setMoney(b); }
-    public void setJailFreeCard(boolean state) { jailFreeCard = state; }
+    public void setBal(int b) { bal.setMoney(b); }
 
-    // toString
+    //toString
     public String toString() {
         String output = "Name: " + getName() +
                         "\nAge: " + getAge() +
@@ -60,8 +57,8 @@ public class Player {
             return false;
         }
         int price = property.getPrice();
-        int currentBalance = playerBank.getMoney();
-        playerBank.setMoney(currentBalance-price);
+        int currentBalance = bal.getMoney();
+        bal.setMoney(currentBalance-price);
         property.setIsOwned(true);
         ownedProperty[numberPropertiesOwned] = property;
         numberPropertiesOwned++;
@@ -70,9 +67,14 @@ public class Player {
 
     public void players (int antalSpillere){
            Player[] player = new Player[antalSpillere];
+           //player.
         }
 
-
+    public void startBal (int spiller){
+        if (spiller == 2) { setBal(20); }
+        else if (spiller == 3){ setBal(18); }
+        else { setBal(16); }
+    }
 
 
 }
